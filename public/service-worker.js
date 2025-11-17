16c61d526278f9d489651c8dd5fc1e3651aa824f
jhonatanYK <jhonatan.kaneda@edu.unipar.br>
2025-11-17 18:10:21 -0300

    r

---
+++ public/service-worker.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'gestor-agro-v1';
+const urlsToCache = [
+  '/',
+  '/dashboard',
+  '/styles.css',
+  '/tailwind.css',
+  '/trator.png'
+];
+
+// Instalação do Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Ativação do Service Worker
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Intercepta requisições
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Retorna do cache se disponível, senão busca na rede
+        return response || fetch(event.request);
+      })
+  );
+});
